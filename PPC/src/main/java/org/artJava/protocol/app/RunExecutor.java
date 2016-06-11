bb44a0490f8427b499812874afa4396e79926458
artJava <artJava@192.168.56.1>
2016-06-11 20:52:16 +0800

    artJava

--- PPC/src/main/java/org/artJava/protocol/app/RunExecutor.java
+++ PPC/src/main/java/org/artJava/protocol/app/RunExecutor.java
@@ -1,15 +1,11 @@
 package org.artJava.protocol.app;
 
-import java.io.IOException;
-
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.GnuParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Options;
-import org.artJava.protocol.config.SimpleConfig;
-import org.artJava.protocol.config.SimpleConfigBuilder;
-import org.artJava.protocol.http.service.NodeMasterHttpService;
-
 public class RunExecutor {
+
+	public static void main(String[] args) {
+
+		Application.main(new String[] { "-e" });
+
+	}
 
 }
